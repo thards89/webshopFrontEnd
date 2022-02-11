55ce6461ad0de9810aba8fa1d6378f0acff2a32b
Elioness <lmeraki@yahoo.com>
2022-02-11 09:59:02 +0100

    Ready for pulling

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -4,6 +4,27 @@ const Login = () => {
   return (
     <div>
       <h4>User Login </h4>
+      <form action="/login" method="post" id="login">
+        <div>
+          <label for="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Enter your email address"
+          />
+        </div>
+        <div>
+          <label for="name">Password:</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
